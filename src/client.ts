68786cdeb46be35d2335ea6ff3cd36c123cfcb8f
Adam Williams <adam.williams@lifeomic.com>
2020-08-02 16:51:31 -0400

    Add validateInvocation test

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,5 @@
+import http from 'http';
+
 import { IntegrationProviderAuthenticationError } from '@jupiterone/integration-sdk-core';
 
 import { IntegrationConfig } from './types';
@@ -16,14 +18,34 @@ export class APIClient {
   constructor(readonly config: IntegrationConfig) {}
 
   public async verifyAuthentication(): Promise<void> {
+    // TODO make the most light-weight request possible to validate
+    // authentication works with the provided credentials, throw an err if
+    // authentication fails
+    const request = new Promise((resolve, reject) => {
+      http.get(
+        {
+          hostname: 'localhost',
+          port: 443,
+          path: '/api/v1/some/endpoint?limit=1',
+          agent: false,
+          timeout: 10,
+        },
+        (res) => {
+          if (res.statusCode !== 200) {
+            reject(new Error('Provider authentication failed'));
+          } else {
+            resolve();
+          }
+        },
+      );
+    });
+
     try {
-      // TODO make the most light-weight request possible to validate
-      // authentication works with the provided credentials, throw an err if
-      // authentication fails
+      await request;
     } catch (err) {
       throw new IntegrationProviderAuthenticationError({
         cause: err,
-        endpoint: 'https://provider.com/api/v1/some/endpoint?limit=1',
+        endpoint: 'https://localhost/api/v1/some/endpoint?limit=1',
         status: err.status,
         statusText: err.statusText,
       });
