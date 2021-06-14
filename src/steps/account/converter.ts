d8cf79d6a7a6aa1bcf6120f1e799e94e9e13f315
Chris Elias <chris.elias5@gmail.com>
2021-06-14 17:06:54 -0400

    Removed unused import

--- src/steps/account/converter.ts
+++ src/steps/account/converter.ts
@@ -3,7 +3,7 @@ import {
   Entity,
 } from '@jupiterone/integration-sdk-core';
 
-import { Steps, Entities } from '../constants';
+import { Entities } from '../constants';
 
 export function createAccountEntity(): Entity {
   return createIntegrationEntity({
