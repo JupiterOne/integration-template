8a3d618f786a87f2b44ac4cb4f684148d744343a
Adam Pierson <adam.pierson@jupiterone.com>
2021-11-10 15:06:06 -0600

    Addressing PR comments.

--- src/steps/index.test.ts
+++ src/steps/index.test.ts
@@ -1,111 +1,82 @@
-import { createMockStepExecutionContext } from '@jupiterone/integration-sdk-testing';
-
-import { IntegrationConfig } from '../config';
 import { buildGroupUserRelationships, fetchGroups, fetchUsers } from './access';
 import { fetchAccountDetails } from './account';
 import { integrationConfig } from '../../test/config';
-import { Recording, setupIntegrationRecording } from '../../test/recording';
+import { createDataCollectionTest } from '../../test/recording';
 
-let recording: Recording;
+import { Entities } from './constants';
 
-function isRecordingEnabled() {
-  return Boolean(process.env.LOAD_ENV) === true;
-}
-
-test('should collect data', async () => {
-  const context = createMockStepExecutionContext<IntegrationConfig>({
-    instanceConfig: integrationConfig,
-  });
-
-  recording = setupIntegrationRecording({
-    directory: __dirname,
-    name: 'should collect data',
-    options: {
-      mode: isRecordingEnabled() ? 'record' : 'replay',
-    },
-  });
-
-  // Simulates dependency graph execution.
-  // See https://github.com/JupiterOne/sdk/issues/262.
-  await fetchAccountDetails(context);
-  await fetchUsers(context);
-  await fetchGroups(context);
-  await buildGroupUserRelationships(context);
-
-  // Review snapshot, failure is a regression
-  expect({
-    numCollectedEntities: context.jobState.collectedEntities.length,
-    numCollectedRelationships: context.jobState.collectedRelationships.length,
-    collectedEntities: context.jobState.collectedEntities,
-    collectedRelationships: context.jobState.collectedRelationships,
-    encounteredTypes: context.jobState.encounteredTypes,
-  }).toMatchSnapshot();
-
-  const accounts = context.jobState.collectedEntities.filter((e) =>
-    e._class.includes('Account'),
-  );
-  expect(accounts.length).toBeGreaterThan(0);
-  expect(accounts).toMatchGraphObjectSchema({
-    _class: ['Account'],
-    schema: {
-      additionalProperties: false,
-      properties: {
-        _type: { const: 'acme_account' },
-        manager: { type: 'string' },
-        _rawData: {
-          type: 'array',
-          items: { type: 'object' },
+describe('#fetchDetails', () => {
+  test('should collect data', async () => {
+    await createDataCollectionTest({
+      recordingName: 'fetchDetails',
+      recordingDirectory: __dirname,
+      integrationConfig,
+      stepFunctions: [
+        fetchAccountDetails,
+        fetchUsers,
+        fetchGroups,
+        buildGroupUserRelationships,
+      ],
+      entitySchemaMatchers: [
+        {
+          _type: Entities.ACCOUNT._type,
+          matcher: {
+            _class: ['Account'],
+            schema: {
+              additionalProperties: false,
+              properties: {
+                _type: { const: 'acme_account' },
+                manager: { type: 'string' },
+                _rawData: {
+                  type: 'array',
+                  items: { type: 'object' },
+                },
+              },
+              required: ['manager'],
+            },
+          },
         },
-      },
-      required: ['manager'],
-    },
-  });
-
-  const users = context.jobState.collectedEntities.filter((e) =>
-    e._class.includes('User'),
-  );
-  expect(users.length).toBeGreaterThan(0);
-  expect(users).toMatchGraphObjectSchema({
-    _class: ['User'],
-    schema: {
-      additionalProperties: false,
-      properties: {
-        _type: { const: 'acme_user' },
-        firstName: { type: 'string' },
-        _rawData: {
-          type: 'array',
-          items: { type: 'object' },
+        {
+          _type: Entities.GROUP._type,
+          matcher: {
+            _class: ['UserGroup'],
+            schema: {
+              additionalProperties: false,
+              properties: {
+                _type: { const: 'acme_group' },
+                logoLink: {
+                  type: 'string',
+                  // Validate that the `logoLink` property has a URL format
+                  format: 'url',
+                },
+                _rawData: {
+                  type: 'array',
+                  items: { type: 'object' },
+                },
+              },
+              required: ['logoLink'],
+            },
+          },
         },
-      },
-      required: ['firstName'],
-    },
-  });
-
-  const userGroups = context.jobState.collectedEntities.filter((e) =>
-    e._class.includes('UserGroup'),
-  );
-  expect(userGroups.length).toBeGreaterThan(0);
-  expect(userGroups).toMatchGraphObjectSchema({
-    _class: ['UserGroup'],
-    schema: {
-      additionalProperties: false,
-      properties: {
-        _type: { const: 'acme_group' },
-        logoLink: {
-          type: 'string',
-          // Validate that the `logoLink` property has a URL format
-          format: 'url',
+        {
+          _type: Entities.USER._type,
+          matcher: {
+            _class: ['User'],
+            schema: {
+              additionalProperties: false,
+              properties: {
+                _type: { const: 'acme_user' },
+                firstName: { type: 'string' },
+                _rawData: {
+                  type: 'array',
+                  items: { type: 'object' },
+                },
+              },
+              required: ['firstName'],
+            },
+          },
         },
-        _rawData: {
-          type: 'array',
-          items: { type: 'object' },
-        },
-      },
-      required: ['logoLink'],
-    },
+      ],
+    });
   });
-
-  if (recording) {
-    await recording.stop();
-  }
 });
--- test/recording.ts
+++ test/recording.ts
@@ -1,36 +1,182 @@
+import { IntegrationStepExecutionContext } from '@jupiterone/integration-sdk-core';
 import {
   Recording,
   setupRecording,
   SetupRecordingInput,
-  mutations,
+  createMockStepExecutionContext,
+  ToMatchGraphObjectSchemaParams,
+  ToMatchRelationshipSchemaParams,
 } from '@jupiterone/integration-sdk-testing';
+import * as nodeUrl from 'url';
+import { PollyConfig } from '@pollyjs/core';
+
+const DEFAULT_RECORDING_HOST = '127.0.0.1:1234';
+const DEFAULT_RECORDING_BASE_URL = `https://${DEFAULT_RECORDING_HOST}`;
 
 export { Recording };
 
-export function setupIntegrationRecording(options: SetupRecordingInput) {
-  return setupRecording({
-    ...options,
-    mutateEntry: (entry) => {
-      mutations.unzipGzippedRecordingEntry(entry);
+interface PollyRequestHeader {
+  name: string;
+  value: string;
+}
 
-      if (/oauth2\/token/.exec(entry.request.url) && entry.request.postData) {
-        // Redact request body with secrets for authentication
-        entry.request.postData.text = '[REDACTED]';
+function redact(entry: any) {
+  const responseText = entry.response.content.text;
+  if (!responseText) {
+    return;
+  }
 
-        // Redact authentication response token
-        const responseText = entry.response.content.text;
-        const responseJson = responseText && JSON.parse(responseText);
-        if (responseJson.access_token) {
-          entry.response.content.text = JSON.stringify(
-            {
-              ...responseJson,
-              access_token: '[REDACTED]',
-            },
-            null,
-            0,
-          );
+  const parsedResponseText = JSON.parse(responseText.replace(/\r?\n|\r/g, ''));
+  entry.response.content.text = JSON.stringify(parsedResponseText);
+}
+
+function getNormalizedRecordingUrl(url: string) {
+  const parsedUrl = nodeUrl.parse(url);
+  return `${DEFAULT_RECORDING_BASE_URL}${parsedUrl.path}`;
+}
+
+function normalizeRequestEntryHeaders(oldRequestHeaders: PollyRequestHeader[]) {
+  const newRequestHeaders: PollyRequestHeader[] = [];
+
+  for (const oldRequestHeader of oldRequestHeaders) {
+    if (oldRequestHeader.name === 'host') {
+      newRequestHeaders.push({
+        ...oldRequestHeader,
+        value: DEFAULT_RECORDING_HOST,
+      });
+    } else {
+      newRequestHeaders.push(oldRequestHeader);
+    }
+  }
+
+  return newRequestHeaders;
+}
+
+function normalizeRequestEntry(entry: any) {
+  entry.request.url = getNormalizedRecordingUrl(entry.request.url);
+  entry.request.headers = normalizeRequestEntryHeaders(
+    entry.request.headers || [],
+  );
+}
+
+function isRecordingEnabled() {
+  return Boolean(process.env.LOAD_ENV) === true;
+}
+
+export async function withRecording(
+  recordingName: string,
+  directoryName: string,
+  cb: () => Promise<void>,
+  options?: SetupRecordingInput['options'],
+) {
+  const recordingEnabled = isRecordingEnabled();
+
+  const recording = setupRecording({
+    directory: directoryName,
+    name: recordingName,
+    mutateEntry(entry) {
+      redact(entry);
+      normalizeRequestEntry(entry);
+    },
+    options: {
+      mode: recordingEnabled ? 'record' : 'replay',
+      recordIfMissing: recordingEnabled,
+      recordFailedRequests: false,
+      // https://github.com/Netflix/pollyjs/blob/cbca602a5a446da46a4a2834f893670b8c577880/docs/configuration.md#matchrequestsby
+      matchRequestsBy: {
+        headers: false,
+        body: false,
+        method: true,
+        order: true,
+        url(url, req) {
+          return getNormalizedRecordingUrl(url);
+        },
+      },
+      ...(options || {}),
+    },
+  });
+
+  try {
+    await cb();
+  } finally {
+    await recording.stop();
+  }
+}
+
+export interface EntitySchemaMatcher {
+  _type: string;
+  matcher: ToMatchGraphObjectSchemaParams;
+}
+
+export interface RelationshipSchemaMatcher {
+  _type: string;
+  matcher: ToMatchRelationshipSchemaParams;
+}
+
+export interface CreateDataCollectionTestParams<IIntegrationConfig> {
+  recordingName: string;
+  recordingDirectory: string;
+  integrationConfig: IIntegrationConfig;
+  stepFunctions: ((
+    context: IntegrationStepExecutionContext<IIntegrationConfig>,
+  ) => Promise<void>)[];
+  entitySchemaMatchers?: EntitySchemaMatcher[];
+  relationshipSchemaMatchers?: RelationshipSchemaMatcher[];
+  options?: PollyConfig;
+}
+
+export async function createDataCollectionTest<IIntegrationConfig>({
+  recordingName,
+  recordingDirectory,
+  integrationConfig,
+  stepFunctions,
+  entitySchemaMatchers,
+  relationshipSchemaMatchers,
+  options,
+}: CreateDataCollectionTestParams<IIntegrationConfig>) {
+  const context = createMockStepExecutionContext<IIntegrationConfig>({
+    instanceConfig: integrationConfig,
+  });
+
+  await withRecording(
+    recordingName,
+    recordingDirectory,
+    async () => {
+      for (const stepFunction of stepFunctions) {
+        await stepFunction(context);
+      }
+
+      expect({
+        numCollectedEntities: context.jobState.collectedEntities.length,
+        numCollectedRelationships:
+          context.jobState.collectedRelationships.length,
+        collectedEntities: context.jobState.collectedEntities,
+        collectedRelationships: context.jobState.collectedRelationships,
+        encounteredTypes: context.jobState.encounteredTypes,
+      }).toMatchSnapshot('jobState');
+
+      if (entitySchemaMatchers) {
+        for (const entitySchemaMatcher of entitySchemaMatchers) {
+          expect(
+            context.jobState.collectedEntities.filter(
+              (e) => e._type === entitySchemaMatcher._type,
+            ),
+          ).toMatchGraphObjectSchema(entitySchemaMatcher.matcher);
+        }
+      }
+
+      if (relationshipSchemaMatchers) {
+        for (const relationshipSchemaMatcher of relationshipSchemaMatchers) {
+          expect(
+            context.jobState.collectedRelationships.filter(
+              (r) => r._type === relationshipSchemaMatcher._type,
+            ),
+          ).toMatchDirectRelationshipSchema(relationshipSchemaMatcher.matcher);
         }
       }
     },
-  });
+    { ...options },
+  );
+
+  return { context };
 }
