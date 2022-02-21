7ced88412ba5b0397d438e42ccc21ff1f2ad723d
Matthew Zember <matthew.zember@jupiterone.com>
2022-02-21 16:23:46 -0500

    Update validateInvocation tests to use recordings

--- src/validateInvocation.test.ts
+++ src/validateInvocation.test.ts
@@ -4,38 +4,73 @@ import {
 } from '@jupiterone/integration-sdk-core';
 import {
   createMockExecutionContext,
+  Recording,
   setupRecording,
 } from '@jupiterone/integration-sdk-testing';
+import { integrationConfig } from '../test/config';
 import { IntegrationConfig, validateInvocation } from './config';
 
-it('requires valid config', async () => {
-  const executionContext = createMockExecutionContext<IntegrationConfig>({
-    instanceConfig: {} as IntegrationConfig,
+describe('#validateInvocation', () => {
+  let recording: Recording;
+
+  afterEach(async () => {
+    if (recording) {
+      await recording.stop();
+    }
   });
 
-  await expect(validateInvocation(executionContext)).rejects.toThrow(
-    IntegrationValidationError,
-  );
-});
+  it('requires valid config', async () => {
+    const executionContext = createMockExecutionContext<IntegrationConfig>({
+      instanceConfig: {} as IntegrationConfig,
+    });
 
-it('auth error', async () => {
-  const recording = setupRecording({
-    directory: '__recordings__',
-    name: 'client-auth-error',
+    await expect(validateInvocation(executionContext)).rejects.toThrow(
+      IntegrationValidationError,
+    );
   });
 
-  recording.server.any().intercept((req, res) => {
-    res.status(401);
+  /**
+   * Testing authorizations can be done with recordings.
+   * To capture failed responses, recordFailedRequests is set to true.
+   */
+  it.skip('auth error', async () => {
+    recording = setupRecording({
+      directory: __dirname,
+      name: 'client-auth-error',
+      // Many authorization failures will return non-200 responses
+      // and `recordFailedRequest: true` is needed to capture these responses
+      options: {
+        recordFailedRequests: true,
+      },
+    });
+
+    const executionContext = createMockExecutionContext({
+      instanceConfig: {
+        clientId: 'INVALID',
+        clientSecret: 'INVALID',
+      },
+    });
+
+    await expect(validateInvocation(executionContext)).rejects.toThrow(
+      IntegrationProviderAuthenticationError,
+    );
   });
 
-  const executionContext = createMockExecutionContext({
-    instanceConfig: {
-      clientId: 'INVALID',
-      clientSecret: 'INVALID',
-    },
-  });
+  /**
+   * Testing a successful authorization can be done with recordings as well
+   */
+  it.skip('validates invocation', async () => {
+    recording = setupRecording({
+      directory: __dirname,
+      name: 'validate-invocation',
+    });
 
-  await expect(validateInvocation(executionContext)).rejects.toThrow(
-    IntegrationProviderAuthenticationError,
-  );
+    // Pass integrationConfig to authenticate with real credentials
+    const executionContext = createMockExecutionContext({
+      instanceConfig: integrationConfig,
+    });
+
+    // successful validateInvocation doesn't throw errors and will be undefined
+    await expect(validateInvocation(executionContext)).resolves.toBeUndefined();
+  });
 });
