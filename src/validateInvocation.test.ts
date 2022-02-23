3b50bc42238548d2ba4780818893f1e881f424ed
Matthew Zember <matthew.zember@jupiterone.com>
2022-02-23 12:44:06 -0500

    Using strings when testing errors. Adding describe blocks to tests.

--- src/validateInvocation.test.ts
+++ src/validateInvocation.test.ts
@@ -1,13 +1,9 @@
 import {
-  IntegrationProviderAuthenticationError,
-  IntegrationValidationError,
-} from '@jupiterone/integration-sdk-core';
-import {
   createMockExecutionContext,
   Recording,
-  setupRecording,
 } from '@jupiterone/integration-sdk-testing';
 import { integrationConfig } from '../test/config';
+import { setupProjectRecording } from '../test/recording';
 import { IntegrationConfig, validateInvocation } from './config';
 
 describe('#validateInvocation', () => {
@@ -19,48 +15,21 @@ describe('#validateInvocation', () => {
     }
   });
 
-  it('requires valid config', async () => {
+  test('requires valid config', async () => {
     const executionContext = createMockExecutionContext<IntegrationConfig>({
       instanceConfig: {} as IntegrationConfig,
     });
 
     await expect(validateInvocation(executionContext)).rejects.toThrow(
-      IntegrationValidationError,
+      'Config requires all of {clientId, clientSecret}',
     );
   });
 
   /**
-   * Testing authorizations can be done with recordings.
-   * To capture failed responses, recordFailedRequests is set to true.
+   * Testing a successful authorization can be done with recordings
    */
-  it.skip('auth error', async () => {
-    recording = setupRecording({
-      directory: __dirname,
-      name: 'client-auth-error',
-      // Many authorization failures will return non-200 responses
-      // and `recordFailedRequest: true` is needed to capture these responses
-      options: {
-        recordFailedRequests: true,
-      },
-    });
-
-    const executionContext = createMockExecutionContext({
-      instanceConfig: {
-        clientId: 'INVALID',
-        clientSecret: 'INVALID',
-      },
-    });
-
-    await expect(validateInvocation(executionContext)).rejects.toThrow(
-      IntegrationProviderAuthenticationError,
-    );
-  });
-
-  /**
-   * Testing a successful authorization can be done with recordings as well
-   */
-  it.skip('validates invocation', async () => {
-    recording = setupRecording({
+  test.skip('successfully validates invocation', async () => {
+    recording = setupProjectRecording({
       directory: __dirname,
       name: 'validate-invocation',
     });
@@ -72,5 +41,64 @@ describe('#validateInvocation', () => {
 
     // successful validateInvocation doesn't throw errors and will be undefined
     await expect(validateInvocation(executionContext)).resolves.toBeUndefined();
+  });
+
+  /* Adding `describe` blocks segments the tests into logical sections
+   * and makes the output of `yarn test --verbose` provide meaningful
+   * to project information to future maintainers.
+   */
+  describe('fails validating invocation', () => {
+    /**
+     * Testing failing authorizations can be done with recordings as well.
+     * For each possible failure case, a test can be made to ensure that
+     * error messaging is expected and clear to end-users
+     */
+    describe('invalid user credentials', () => {
+      test.skip('should throw if clientId is invalid', async () => {
+        recording = setupProjectRecording({
+          directory: __dirname,
+          name: 'client-id-auth-error',
+          // Many authorization failures will return non-200 responses
+          // and `recordFailedRequest: true` is needed to capture these responses
+          options: {
+            recordFailedRequests: true,
+          },
+        });
+
+        const executionContext = createMockExecutionContext({
+          instanceConfig: {
+            clientId: 'INVALID',
+            clientSecret: integrationConfig.clientSecret,
+          },
+        });
+
+        // tests validate that invalid configurations throw an error
+        // with an appropriate and expected message.
+        await expect(validateInvocation(executionContext)).rejects.toThrow(
+          'Provider authentication failed at https://localhost/api/v1/some/endpoint?limit=1: 401 Unauthorized',
+        );
+      });
+
+      test.skip('should throw if clientSecret is invalid', async () => {
+        recording = setupProjectRecording({
+          directory: __dirname,
+          name: 'client-secret-auth-error',
+          options: {
+            recordFailedRequests: true,
+          },
+        });
+
+        const executionContext = createMockExecutionContext({
+          instanceConfig: {
+            clientId: integrationConfig.clientSecret,
+            clientSecret: 'INVALID',
+          },
+        });
+
+        await expect(validateInvocation(executionContext)).rejects.toThrow(
+          'Provider authentication failed at https://localhost/api/v1/some/endpoint?limit=1: 401 Unauthorized',
+        );
+      });
+    });
   });
 });
